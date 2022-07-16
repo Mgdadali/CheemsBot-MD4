ffec456b737d9c47044ee05ed3d2e20031e2cee1
Mgdadali <109249013+Mgdadali@users.noreply.github.com>
2022-07-16 14:07:51 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['249129431348'] //ur owner number
+global.ownername = "Mgdad bot" //ur owner name
+global.ytname = "professional tech " //ur yt chanel name
+global.socialm = "GitHub: Mgdadali" //ur github or insta name
+global.location = "sudan 🇸🇩" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['249129431348']
+global.ownertag = '249129431348' //ur tag number
+global.botname = 'Cheems Bot MD V4 ✔' //ur bot name
+global.linkz = "https://youtu.be/Y_63fLfyjQc" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/Y_63fLfyjQc" //ur website to be displayed
+global.botscript = 'https://github.com/Mgdadali/CheemsBot-MD4/edit/master/settings.js' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = " Mgdad bot " //ur sticker watermark packname
+global.author = "تم بواسطة بوت مقداد" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['249129431348'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -116,8 +116,7 @@ global.mess = {
     linkm: 'Where is the link?',
     error: 'Error!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    ban: 'تم حظرك من استخدام البوت راسل المالك ،.',',
     banChat: 'The bot was banned in this group, please contact the owner to unban'
 }
     global.limitawal = {
